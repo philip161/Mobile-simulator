a837994da9b812bffac4b5b8e4133e593e994cb1
philip161 <philip161@gmail.com>
2013-04-14 04:43:15 -0300

    Made the vehicle id an index
    Every time you instantiate a vehicle it will automatically increment the id with the help of a static member.

--- src/mobile_simulator/models/Vehicle.java
+++ src/mobile_simulator/models/Vehicle.java
@@ -1,12 +1,16 @@
 package mobile_simulator.models;
 
 public class Vehicle {
-	
-	private int id;
+    public static int staticVehicleId = 0;
+	public int vehicleId = 0;	
 	
 	public enum Direction{
 		
 	}
 	private Direction direction;
-
+	
+	Vehicle() {
+		staticVehicleId++;
+		vehicleId = staticVehicleId;
+    }
 }
