c405f684dc084ff11aad7ee4e3213507976208df
philip161 <philip161@gmail.com>
2013-04-14 04:55:49 -0300

    Another field and constructor for Vehicle

--- src/mobile_simulator/models/Vehicle.java
+++ src/mobile_simulator/models/Vehicle.java
@@ -2,15 +2,24 @@ package mobile_simulator.models;
 
 public class Vehicle {
     public static int staticVehicleId = 0;
-	public int vehicleId = 0;	
+	public int vehicleId = 0;
+	public int startTick;
 	
 	public enum Direction{
 		
 	}
 	private Direction direction;
 	
+	
 	Vehicle() {
 		staticVehicleId++;
 		vehicleId = staticVehicleId;
+		startTick = -1;
+    }	
+	
+	Vehicle( int tick ) {
+		staticVehicleId++;
+		vehicleId = staticVehicleId;
+		startTick = tick;
     }
 }
